ed19659aea64c8a6c17c5effafbe41adcf52b8cf
Jesus <jmodestov@gmail.com>
2020-09-09 18:08:28 -0700

    Removed node-sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 const path = require('path');
 
 module.exports = {
-  //Webpack will use built-in optimization accordingly
+  // Webpack will use built-in optimization accordingly
   mode: process.env.NODE_ENV,
 
-  //Webpack starts bundlinng at index.js. Recursively builds dependancy graph. 
-  entry: "./client/index.js",
+  // Webpack starts bundlinng at index.js. Recursively builds dependancy graph.
+  entry: './client/index.js',
 
   output: {
-    //bundling results will be emitted to this path
-    path: path.resolve(__dirname, 'bundle'),
-    filename: 'bundle.js'
+    // bundling results will be emitted to this path
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
   },
 
-  //configuration regarding modules
+  // configuration regarding modules
   module: {
     rules: [
       {
@@ -22,10 +22,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
-      }, 
+      },
       {
         test: /\.s?css/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
@@ -33,19 +33,15 @@ module.exports = {
     ],
   },
 
-  //devServer configuration
+  // devServer configuration
   devServer: {
     publicPath: '/build/',
     proxy: {
       '/api': 'http://localhost:3000',
-    }
+    },
   },
-  
 };
-
-
 
 /*
 Depencies: "express": "^4.17.1",    "fs-extra": "^9.0.1",    "path": "^0.12.7",    "react": "^16.13.1",    "react-dom": "^16.13.1"
-
 */
