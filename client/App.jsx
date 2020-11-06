e9a1e37d88f5a469825858c348f55625f256cf91
Jesus <jmodestov@gmail.com>
2020-11-05 22:07:29 -0800

    Removed unnecessary mapStateToProps in App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -11,45 +11,29 @@ import * as actions from './actions/actions';
 
 // create connection to mapStateToProps and mapDispatchToProps
 
-const mapStateToProps = (state) => ({
-  jobsApplied: state.jobApps.jobsApplied,
-  newCompany: state.jobApps.newCompany,
-  newPosition: state.jobApps.newPosition,
-  newDate: state.jobApps.newDate,
-  newStatus: state.jobApps.newStatus,
-  // addJob is true if user wants to add a job entry
-  addJobCard: state.jobApps.addJobCard,
-});
+// const mapStateToProps = (state) => ({
+//   jobsApplied: state.jobApps.jobsApplied,
+//   newCompany: state.jobApps.newCompany,
+//   newPosition: state.jobApps.newPosition,
+//   newDate: state.jobApps.newDate,
+//   newStatus: state.jobApps.newStatus,
+//   // addJob is true if user wants to add a job entry
+//   addJobCard: state.jobApps.addJobCard,
+// });
 
 const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch);
 
 // Work on PropType validation.
 
 // Include Route when signup page is created.
-const App = (props) => {
-  const {
-    deleteJob,
-    addJob,
-    loadApps,
-    jobsApplied,
-    addJobCard,
-    newCompany,
-    newPosition,
-    newDate,
-    newStatus,
-    updateNewCompany,
-    updateNewPosition,
-    updateNewDate,
-    updateNewStatus,
-    saveNewJob,
-  } = props;
+const App = ({ addJob }) => {
   return (
     <div id="app">
       <NavBar />
-      <ViewOptions addJob={props.addJob} />
+      <ViewOptions addJob={addJob} />
       <MainDisplay />
     </div>
   );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapDispatchToProps)(App);
